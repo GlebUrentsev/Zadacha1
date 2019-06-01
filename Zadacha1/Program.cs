b89d2e382f05ed0db5cf6f23064f2b072b7a312c
Gleb <Gleb-urencev@yandex.ru>
2019-06-01 10:16:09 +0500

    Code changes

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace Zadacha1
 {
@@ -10,25 +12,42 @@ namespace Zadacha1
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            Agent[] list = new Agent[n];
+            string line="", line1 = "";
+            StreamReader tr = new StreamReader("input.txt");
+            StreamWriter sw = new StreamWriter("output.txt");
+            while (!tr.EndOfStream)
+            {
+                line = tr.ReadLine();
+                line1 = tr.ReadLine();
+            }
+            tr.Close();
+            string[] s = line1.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int n = Convert.ToInt32(line);
+            List<int> ages = new List<int>();
+            List<int> riskes = new List<int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (Convert.ToInt32(s[i]) >= 5000) ages.Add(Convert.ToInt32(s[i]));
+                else riskes.Add(Convert.ToInt32(s[i]));
+            }
+            List<Agent> list = new List<Agent>();
             Agent agent;
             for (int i = 0; i < n; i++)
             {
                 int age, risk;
-                age = int.Parse(Console.ReadLine());
-                risk = int.Parse(Console.ReadLine());
+                age = ages[i];
+                risk = riskes[i];
                 agent = new Agent(age, risk);
-                list[i] = agent;
+                list.Add(agent);
             }
-            Array.Sort(list);
+            list.Sort();         
             int[] minRisk = new int[n];
             minRisk[1] = list[1].Risk;
-            if (2 <n)       
+            if (2 < n)
             {
                 minRisk[2] = list[1].Risk + list[2].Risk;
             }
-            if (3<n)
+            if (3 < n)
             {
                 minRisk[3] = list[3].Risk + list[1].Risk;
             }
@@ -36,7 +55,8 @@ namespace Zadacha1
             {
                 minRisk[last] = Math.Min(minRisk[last - 2] + list[last].Risk, minRisk[last - 3] + list[last - 1].Risk + list[last].Risk);
             }
-            Console.WriteLine(minRisk[n-1]);          
+            sw.WriteLine(minRisk[n - 1]);
+            sw.Close();
         }
     }
 
